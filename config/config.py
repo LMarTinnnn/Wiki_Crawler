5d5959dafbdae9a74503be428d4563df44d4379a
LMarTinnnn <liuyongbo000@icloud.com>
2016-12-08 23:57:55 +0800

    整理忽略文件
    刚发现 .gitignore…

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 headers = {
-    'User_Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/54.0.2840.98 Safari/537.36'
 }
 
